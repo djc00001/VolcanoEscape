40b84bcec2f54e896101cde31e3b8e4a9c09852e
David Clark <djc00001@mix.wvu.edu>
2024-04-14 21:40:02 -0400

    Working Parkour

---
+++ Volcano Escape/Assets/Scripts/Timer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] float remainingTime;
+
+    private void OnEnable()
+    {
+//        remainingTime = 60f; // Reset remainingTime to 60 seconds when object is enabled
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (remainingTime > 0)
+        {
+            remainingTime -= Time.deltaTime;
+        }
+        else if (remainingTime < 0)
+        {
+            remainingTime = 0;
+        }
+        int minutes = Mathf.FloorToInt(remainingTime / 60);
+        int seconds = Mathf.FloorToInt(remainingTime % 60);
+
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
